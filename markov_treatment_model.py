d6b781205f0b91c4363245938e767eaa7dd6ed55
Matthew Eng <matthew.eng2@gmail.com>
2021-07-31 11:58:33 -0400

    Cycle through costs, membership, utility and produce dict with results

--- markov_treatment_model.py
+++ markov_treatment_model.py
@@ -1,11 +1,8 @@
 import numpy as np
+import pandas as pd
 
 CYCLES = 12
-STATE_MEMBERSHIP = {
-    'healthy': 1000,
-    'diseased': 0,
-    'dead': 0,
-}
+STATE_MEMBERSHIP = [1000,0,0] # healthy, diseased, dead
 
 treatment_matrix = {
     'a': [[0.7,0.2,0.1],
@@ -23,15 +20,31 @@ payoffs = {
             'utility': [0.8,0.5,0]},
 }
 
-def cycle_members(membership, treatment_matrix):
+def calc_members(membership, treatment_matrix):
     return np.dot(membership, treatment_matrix)
 
-def cycle_costs(membership, costs):
-    return np.multiply(membership, costs)
+def calc_costs(membership, costs):
+    return np.multiply(membership, costs).tolist()
 
-def cycle_qalys(membership, qalys):
-    return np.multiply(membership, qalys)
+def calc_qalys(membership, qalys):
+    return np.multiply(membership, qalys).tolist()
 
-print(cycle_members([1000,0,0],treatment_matrix['a']))
-print(cycle_costs([1000,0,0], payoffs['a']['costs']))
-print(cycle_qalys([1000,0,0], payoffs['a']['utility']))
+# Cycle 0
+memberships = [STATE_MEMBERSHIP]
+costs = [payoffs['a']['costs']]
+qalys = [payoffs['a']['utility']]
+cycle_membership, cycle_costs, cycle_qalys = STATE_MEMBERSHIP, costs, qalys
+
+for cycle in range(1, CYCLES):
+    cycle_membership = calc_members(cycle_membership,treatment_matrix['a'])
+    memberships.append(cycle_membership)
+    cycle_costs = calc_costs(cycle_membership, payoffs['a']['costs'])
+    costs.append(cycle_costs)
+    cycle_qalys = calc_qalys(cycle_membership, payoffs['a']['utility'])
+    qalys.append(cycle_qalys)
+
+result = {
+    'membership': memberships,
+    'costs': costs,
+    'qalys': qalys,
+}
